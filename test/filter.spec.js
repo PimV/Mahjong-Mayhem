dc9bcec73d1505106acbde4d7d8fee446c493255
ninox92 <thomasreynders@hotmail.nl>
2015-06-16 21:01:21 +0200

    FILTER FINAL

--- test/filter.spec.js
+++ test/filter.spec.js
@@ -29,40 +29,43 @@ describe("Game state Filter", function() {
 		stateFilter = $injector.get('$filter')('stateFilter');
 	}));
 
-	it("should filter on state 'open' ", function(done){
-		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+	it("should filter on state 'open'", function(done){
+		//Act
 		var result = stateFilter(input, states.open);
-
+		
+		//Assert
 		expect(result).to.have.length(1);
 		expect(result[0].id).to.equal(1);
 
 		done();
 	});
 
-	it("should filter on state 'playing' ", function(done){
-		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+	it("should filter on state 'playing'", function(done){
+		//Act
 		var result = stateFilter(input, states.playing);
-
+		//Assert
 		expect(result).to.have.length(1);
 		expect(result[0].id).to.equal(2);
 
 		done();
 	});
 
-	it("should filter on state 'finished' ", function(done){
-		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+	it("should filter on state 'finished'", function(done){
+		//Act
 		var result = stateFilter(input, states.finished);
 
+		//Assert
 		expect(result).to.have.length(1);
 		expect(result[0].id).to.equal(3);
 
 		done();
 	});
 
-	it("should return the previous input ", function(done){
-		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+	it("should return the previous input", function(done){
+		//Act
 		var result = stateFilter(input, states.none);
 
+		//Assert
 		expect(result).to.equal(input);
 
 		done();
